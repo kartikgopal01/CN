939e0da621804c26804e8c3433de73737f00ee34
kartikgopal01 <kartikgopal01@gmail.com>
2024-12-13 22:58:09 +0530

    Error solved
    	modified:   NetworkSimulation.class
    	modified:   NetworkSimulation.java
    	modified:   Node.class
    	modified:   Sender.class
    	new file:   ShortestPathRouting$Graph.class
    	new file:   ShortestPathRouting$PathVectorRouting.class
    	modified:   ShortestPathRouting.class
    	modified:   ShortestPathRouting.java
    	modified:   SlidingWindowProtocol.class
    	modified:   SlidingWindowProtocol.java

--- NetworkSimulation.java
+++ NetworkSimulation.java
@@ -1,49 +1,26 @@
-import java.util.LinkedList;
-import java.util.Queue; 
-import java.util.Random; 
+import java.util.*;
 
-class Node { 
-    Queue<Integer> queue = new LinkedList<>(); 
-    int queueSize, packetsDropped = 0; 
+class Node {
+    Queue<Integer> queue = new LinkedList<>();
+    int queueSize, packetsDropped = 0;
 
-    Node(int size) { 
-        queueSize = size; 
-    } 
+    Node(int size) { queueSize = size; }
 
-    void sendPing(int packetId) { 
-        if (queue.size() < queueSize) { 
-            queue.add(packetId); 
-        } else { 
-            packetsDropped++; 
-        } 
-    } 
+    void sendPing(int packetId) { if (queue.size() < queueSize) queue.add(packetId); else packetsDropped++; }
 
-    void receivePing() { 
-        if (!queue.isEmpty()) queue.poll(); 
-    } 
-} 
+    void receivePing() { if (!queue.isEmpty()) queue.poll(); }
+}
 
-public class NetworkSimulation { 
-    public static void main(String[] args) { 
-        int numNodes = 6, queueSize = 2, pingsToSend = 300; 
-        Node[] nodes = new Node[numNodes]; 
-        Random rand = new Random(); 
+public class NetworkSimulation {
+    public static void main(String[] args) {
+        int numNodes = 6, queueSize = 2, pingsToSend = 300;
+        Node[] nodes = new Node[numNodes];
+        Random rand = new Random();
 
+        for (int i = 0; i < numNodes; i++) nodes[i] = new Node(queueSize);
+        for (int i = 0; i < pingsToSend; i++) nodes[rand.nextInt(numNodes)].sendPing(i);
+        for (Node node : nodes) for (int i = 0; i < 10; i++) node.receivePing();
         for (int i = 0; i < numNodes; i++) 
-            nodes[i] = new Node(queueSize); 
-
-        for (int i = 0; i < pingsToSend; i++) { 
-            int sender = rand.nextInt(numNodes); 
-            nodes[sender].sendPing(i); 
-        } 
-
-        for (Node node : nodes) { 
-            for (int i = 0; i < 10; i++) { 
-                node.receivePing(); 
-            } 
-        } 
-
-        for (int i = 0; i < numNodes; i++)  
-            System.out.println("Node " + i + " dropped packets: " + nodes[i].packetsDropped); 
-    } 
+            System.out.println("Node " + i + " dropped packets: " + nodes[i].packetsDropped);
+    }
 }
